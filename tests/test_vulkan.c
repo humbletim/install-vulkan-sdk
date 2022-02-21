17ba98c065a1459d2d73c225c6235ee79e05a7d0
humbletim <humbletim@gmail.com>
2022-02-21 01:01:52 -0500

    add compile tests/

---
+++ tests/test_vulkan.c
@@ -0,0 +1,20 @@
+#include <vulkan/vulkan.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char** argv) {
+  PFN_vkEnumerateInstanceVersion vkEnumerateInstanceVersion = (PFN_vkEnumerateInstanceVersion)vkGetInstanceProcAddr(NULL, "vkEnumerateInstanceVersion");
+  if(vkEnumerateInstanceVersion) {
+      vkEnumerateInstanceVersion(&version);
+  } else {
+      fprintf(stderr, "vkGetInstanceProcAddr (%p) lookup of vkEnumerateInstanceVersion failed...\n", vkEnumerateInstanceVersion);
+      return 1;
+  }
+
+  printf("Vulkan Version: (%u) %d.%d.%d\n", version,
+    VK_VERSION_MAJOR(version),
+    VK_VERSION_MINOR(version),
+    VK_VERSION_PATCH(version)
+  );
+  return 0;
+}
