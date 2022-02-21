d81299c185d187fec04a8dc5673e500c1e98e4d3
humbletim <humbletim@gmail.com>
2022-02-21 00:52:42 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# install-vulkan-sdk v1
+
+[![test install-vulkan-sdk](https://github.com/humbletim/install-vulkan-sdk/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/humbletim/install-vulkan-sdk/actions/workflows/ci.yml)
+
+This action automatically downloads and installs the Vulkan SDK development environment.
+
+### Usage
+
+```yaml
+  -name: Install Vulkan SDK
+   uses: humbletim/install-vulkan-sdk@v1
+   with:
+     version: 1.3.204.0
+     cache: true
+```
+
+Parameters:
+- *version* (required): N.N.N.N style Vulkan SDK release number.
+- *cache* (optional; default=false): boolean indicating whether to cache the downloaded installer file between builds.
+
+### SDK Revisions
+
+Several recent SDK releases (known to have installers available for all three windows/mac/linux platforms):
+- 1.2.170.0
+- 1.2.189.0
+- 1.2.198.1
+- 1.3.204.0
+
+Additional release numbers can be found at https://vulkan.lunarg.com/sdk/home.
+
+### Environment
+
+Exported variables:
+- `VULKAN_SDK` (standard variable used by cmake and other build tools)
+- `VULKAN_SDK_VERSION`
+- `VULKAN_SDK_PLATFORM`
+- `PATH` is extended to include `VULKAN_SDK/bin` (so SDK tools like `glslangValidator` can be used directly)
+
+### Caveats
+
+Please be aware that Vulkan SDKs can use a lot of disk space -- recently reported 1.3.204.0 installation sizes:
+  - windows: 617M
+  - linux: 631M
+  - mac: 1.8G (1.3G of that being `lib/libshaderc_combined.a`)
+
+If your project only depends on Vulkan-Headers and Vulkan-Loader to compile and link against then you may want to consider using [humbletim/setup-vulkan-sdk](https://github.com/humbletim/setup-vulkan-sdk) instead, which allows building individual SDK components directly from Khronos source repos (and uses less disk space).
+
+## References
+- [Vulkan SDK](https://www.lunarg.com/vulkan-sdk/)
+- [Vulkan SDK web services API](https://vulkan.lunarg.com/content/view/latest-sdk-version-api)
+- [humbletim/setup-vulkan-sdk](https://github.com/humbletim/setup-vulkan-sdk)
---
+++ vulkan_prebuilt_helpers.sh
@@ -0,0 +1,51 @@
+# helper functions for downloading/installing platform-specific Vulkan SDKs
+# originally meant for use from GitHub Actions
+#   see: https://github.com/humbletim/install-vulkan-sdk
+# -- humbletim 2022.02
+
+# example of running manually:
+# $ . vulkan_prebuilt_helpers.
+# $ VULKAN_SDK_VERSION=1.3.204.0 download_linux    # fetches vulkan_sdk.tar.gz
+# $ VULKAN_SDK=$PWD/VULKAN_SDK install_linux       # installs
+
+function download_linux() {
+  test -f vulkan_sdk.tar.gz || curl -s -L -o vulkan_sdk.tar.gz https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VERSION/linux/vulkan_sdk.tar.gz?Human=true
+}
+
+function install_linux() {
+  test -d $VULKAN_SDK && test -f vulkan_sdk.tar.gz
+  echo "extract just the SDK's prebuilt binaries ($VULKAN_SDK_VERSION/x86_64) from vulkan_sdk_.tar.gz into $VULKAN_SDK" >&2
+  tar -C "$VULKAN_SDK" --strip-components 2 -xf vulkan_sdk_.tar.gz $VULKAN_SDK_VERSION/x86_64
+}
+
+function download_windows() {
+  test -f vulkan_sdk.exe || curl -s -L -o vulkan_sdk.exe https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VERSION/windows/vulkan_sdk.exe?Human=true
+}
+
+function install_windows() {
+  test -d $VULKAN_SDK && test -f vulkan_sdk.exe
+  7z x vulkan_sdk.exe -aoa -o$VULKAN_SDK
+  rm -v vulkan_sdk.exe
+}
+
+function download_mac() {
+  test -f vulkan_sdk.dmg || curl -s -L -o vulkan_sdk.dmg https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VERSION/mac/vulkan_sdk.dmg?Human=true
+}
+
+function install_mac() {
+  test -d $VULKAN_SDK && test -f vulkan_sdk.dmg
+  local mountpoint=$(hdiutil attach vulkan_sdk.dmg | grep vulkansdk | awk 'END {print $NF}')
+  if [[ -d $mountpoint ]] ; then
+    echo "mounted dmg image: 'vulkan_sdk.dmg' (mountpoint=$mountpoint)" >&2
+  else
+    echo "could not mount dmg image: vulkan_sdk.exe (mountpoint=$mountpoint)" >&2
+    exit 7
+  fi
+  local sdk_temp=$VULKAN_SDK.tmp
+  sudo $mountpoint/InstallVulkan.app/Contents/MacOS/InstallVulkan --root "$sdk_temp" --accept-licenses --default-answer --confirm-command install
+  du -hs $sdk_temp
+  cp -r $sdk_temp/macOS/* $VULKAN_SDK/
+  hdiutil detach $mountpoint
+  sudo rm -rf "$sdk_temp"
+  rm -v vulkan_sdk.dmg
+}
